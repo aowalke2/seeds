1511edc6b218809fbf8450ae90c05b0afc16f166
Aza Walker <azawalker.jr@gmail.com>
2025-07-19 13:58:40 -0400

    implemented some functions

--- linkedlist.c
+++ linkedlist.c
@@ -1,4 +1,47 @@
-#include <linkedlist.h>
+#include "linkedlist.h"
+
 #include <stdio.h>
 
-int main(void) {}
+void insert_at_front(LinkedList* list, int value) {
+    Node* node = new_node(value);
+    Node* next = list->head->next;
+    node->next = next;
+    list->head->next = node;
+    list->size++;
+}
+
+// void insert_at_end(LinkedList* list, int value);
+// void insert(LinkedList* list, int value, int index);
+// void delete(LinkedList* list, int index);
+
+int length(LinkedList* list) { return list->size; }
+
+void print_list(LinkedList* list) {
+    Node* curr = list->head->next;
+    while (curr != NULL) {
+        printf("%d->", curr->value);
+        curr = curr->next;
+    }
+    printf("NULL\n");
+}
+
+void free_list(LinkedList* list) {
+    Node* curr = list->head;
+    Node* next;
+    while (curr != NULL) {
+        next = curr->next;
+        free(curr);
+        curr = next;
+    }
+    list->head = NULL;
+    free(list);
+}
+
+int main(void) {
+    LinkedList* list = new_linked_list();
+    insert_at_front(list, 12);
+    insert_at_front(list, 13);
+    print_list(list);
+    printf("length: %d\n", length(list));
+    free(list);
+}
--- linkedlist.h
+++ linkedlist.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Node {
     int value;
@@ -9,6 +10,7 @@ Node* new_node(int value) {
     Node* node = (Node*)malloc(sizeof(Node));
     if (!node) {
         printf("Memory allocation failed\n");
+        exit(1);
     }
 
     node->value = value;
@@ -22,6 +24,20 @@ typedef struct LinkedList {
 } LinkedList;
 
 LinkedList* new_linked_list() {
-    LinkedList* list = {new_node(-1), 0};
+    LinkedList* list = (LinkedList*)malloc(sizeof(LinkedList));
+    if (!list) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    list->head = new_node(-1);
+    list->size = 0;
     return list;
 }
+
+void insert_at_front(LinkedList* list, int value);
+void insert_at_end(LinkedList* list, int value);
+void insert(LinkedList* list, int value, int index);
+void delete(LinkedList* list, int index);
+int length(LinkedList* list);
+void print_list(LinkedList* list);
+void free_list(LinkedList* list);
