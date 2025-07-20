9fd989c8baffbca4ae6e2dc215cbc9a8fd58bda4
Aza Walker <azawalker.jr@gmail.com>
2025-07-20 11:51:22 -0400

    singly linked list finished

--- linkedlist.c
+++ linkedlist.c
@@ -10,9 +10,62 @@ void insert_at_front(LinkedList* list, int value) {
     list->size++;
 }
 
-// void insert_at_end(LinkedList* list, int value);
-// void insert(LinkedList* list, int value, int index);
-// void delete(LinkedList* list, int index);
+void insert_at_end(LinkedList* list, int value) {
+    Node* curr = list->head->next;
+    Node* prev = list->head;
+    while (curr != NULL) {
+        prev = prev->next;
+        curr = curr->next;
+    }
+    prev->next = new_node(value);
+    list->size++;
+}
+
+void insert(LinkedList* list, int value, int index) {
+    if (index < 0 || index > list->size) {
+        printf("Index out of bounds\n");
+        exit(1);
+    }
+
+    if (index == list->size) {
+        insert_at_end(list, value);
+        return;
+    }
+
+    Node* curr = list->head->next;
+    Node* prev = list->head;
+    int count = 0;
+    while (count < index) {
+        count++;
+        prev = prev->next;
+        curr = curr->next;
+    }
+
+    Node* node = new_node(value);
+    prev->next = node;
+    node->next = curr;
+    list->size++;
+}
+
+void delete(LinkedList* list, int index) {
+    if (index < 0 || index >= list->size) {
+        printf("Index out of bounds\n");
+        exit(1);
+    }
+
+    Node* curr = list->head->next;
+    Node* prev = list->head;
+    int count = 0;
+    while (count < index) {
+        count++;
+        prev = prev->next;
+        curr = curr->next;
+    }
+
+    prev->next = curr->next;
+    free(curr);
+    list->size--;
+}
 
 int length(LinkedList* list) { return list->size; }
 
@@ -41,6 +94,15 @@ int main(void) {
     LinkedList* list = new_linked_list();
     insert_at_front(list, 12);
     insert_at_front(list, 13);
+    insert_at_end(list, 5);
+    insert_at_end(list, 7);
+    insert(list, 35, 2);
+    insert(list, 55, 0);
+    insert(list, 20, list->size);
+    print_list(list);
+    printf("length: %d\n", length(list));
+
+    delete(list, 2);
     print_list(list);
     printf("length: %d\n", length(list));
     free(list);
